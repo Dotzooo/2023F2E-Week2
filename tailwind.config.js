91023ae89a5f0bce00f747942e022ba98b83ad3b
Dotzooo <adam.hsu@gogolook.com.tw>
2023-12-05 11:36:14 +0800

    feat color setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,68 @@ module.exports = {
     "./app.{js,ts,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MyColor: {
+          // primary
+          BlueGray: {
+            100: '#F1F5F9',
+            200: '#E2E8F0',
+            300: '#CBD5E1',
+            400: '#94A3B8',
+            600: '#475569',
+            800: '#1E293B',
+            900: '#0F172A'
+          },
+          // secondary
+          Rose: {
+            300: '#FDA4AF',
+            600: '#E11D48'
+          },
+          Blue: {
+            500: '#3B82F6',
+            600: '#2563EB',
+            800: '#1E40AF'
+          },
+          Green: {
+            500: '#22C55E',
+            600: '#16A34A',
+            800: '#166534'
+          },
+          Orange: {
+            500: '#F97316',
+            600: '#EA580C',
+            800: '#9A3412'
+          },
+          Yellow: '#E5BF00',
+          Brown: '#986769',
+          Gray: '#B3B3B3',
+          // background color
+          Bg: {
+            WhiteBlur: '#FFFFFFB2',
+            Search: '#141B1FE5'
+          },
+          // gradient
+          Gradient: {
+            gray: {
+              from: '#94A3B8',
+              via: '#CBD5E1',
+              to: '#CBD5E100'
+            },
+            red: {
+              from: '#E11D48',
+              to: '#FB7185'
+            },
+            red_2: {
+              from: '#F43F5E',
+              via: '#FB7185',
+              via2: '#FDA4AF',
+              to: '#FDA4AF'
+            }
+          },
+        }
+      }
+    },
   },
   plugins: [],
 };
